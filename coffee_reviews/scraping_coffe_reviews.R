7e64307e7c250b19e681599dfc5779357e16fb5f
dcardosos <douglascardoso@usp.br>
2022-06-23 14:31:44 -0300

    get names brand and score and a beautiful log

--- coffee_reviews/scraping_coffe_reviews.R
+++ coffee_reviews/scraping_coffe_reviews.R
@@ -1,6 +1,6 @@
-get_info <- function(.page){
+get_request_info <- function(.page){
   
-  logger::log_info('Page n. {.page}')
+  logger::log_info('`get_request_info` - Page n. {.page}')
   
   response <- glue::glue('https://www.coffeereview.com/advanced-search/page/{.page}') |> 
     httr::GET() |> 
@@ -9,32 +9,62 @@ get_info <- function(.page){
   
   tibble::tibble(
     `urls` = xml2::xml_attr(response, 'href'),
-    `names` = xml2::xml_text(response)
+    `names` = xml2::xml_text(response),
+    `page` = .page
     )
 }
 
-
 monta_tabela <- function(url) {
+  count <<- count + 1
+  logger::log_info('`monta_tabela` - Review n. {count}')
+  html <- xml2::read_html(url)
   
-  #logger::log_info('')
+  names <- html |>  
+    xml2::xml_find_all('//*[@class="review-roaster"]') |> 
+    xml2::xml_text()
+
+  brand <-html |> 
+    xml2::xml_find_all('//*[@class="review-title"]') |>  
+    xml2::xml_text()
   
-  url |> 
-    xml2::read_html() |> 
+  score <- html |> 
+    xml2::xml_find_all('//*[@class="review-template-rating"]') |> 
+    xml2::xml_text()
+  
+  textos <- html |> 
+    xml2::xml_find_all('//*[@class="entry-content"]//p') |> 
+    xml2::xml_text() |> 
+    purrr::reduce(~ paste(..., " - "), .init = "")
+    
+  html |> 
     xml2::xml_find_all('//table') |> 
     rvest::html_table() |> 
     purrr::reduce(~ dplyr::bind_rows(...)) |> 
     tidyr::pivot_wider(
       names_from = X1,
       values_from = X2) |> 
-    janitor::clean_names()
+    janitor::clean_names() |> 
+    dplyr::mutate(
+      names = names,
+      brand = brand,
+      score = score,
+      review_text = textos,
+      url = url)
 }
 
+
 doParallel::registerDoParallel()
-purrr::map_dfr(1:109, get_info) |> 
-  dplyr::pull('urls')|> 
+count <- 0
+purrr::map_dfr(1:109, get_request_info) |> 
+  purrr::pluck('urls')|> 
   purrr::map_dfr(monta_tabela) -> df
 
 
 df |> 
-  readr::write_csv('Documents/mkt/coffe_reviews_infos.csv')
+  dplyr::relocate(
+    names:score,
+    .before = roaster_location)
+
+df |> 
+  readr::write_csv('coffee_reviews/coffe_reviews_infos.csv')
 
